eda660dfa7ebfed06b672982381fb0764470ea22
NRG55 <maloj_1980@yahoo.fr>
2025-01-06 13:34:29 +0000

    Index.js - linked list values are changed.

--- src/index.js
+++ src/index.js
@@ -2,21 +2,23 @@ import LinkedList from "./linkedList.js";
 
 const linkedList = new LinkedList();
 
-linkedList.append("test1");
-linkedList.append("test2");
-linkedList.append("test3");
-linkedList.append("test4");
-linkedList.prepend("test0");
+linkedList.append("cat");
+linkedList.append("parrot");
+linkedList.append("hamster");
+linkedList.append("snake");
+linkedList.append("turtle");
 
-console.log(linkedList);
-console.log(linkedList.size());
-console.log(linkedList.getHead());
-console.log(linkedList.tail());
-console.log(linkedList.at(3));
-linkedList.pop();
-console.log(linkedList);
-console.log(linkedList.contains("test2"));
-console.log(linkedList.find("test3"));
+linkedList.prepend("dog");
+
+// console.log(linkedList);
+// console.log(linkedList.size());
+// console.log(linkedList.getHead());
+// console.log(linkedList.tail());
+// console.log(linkedList.at(3));
+// linkedList.pop();
+// console.log(linkedList);
+console.log(linkedList.contains("snake"));
+console.log(linkedList.find("hamster"));
 console.log(linkedList.toString());
 linkedList.insertAt("Inserted Node", 1);
 linkedList.removeAt(3);
